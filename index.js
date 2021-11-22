60be6e8e9e2b3ce60c8d8af607810c7510b77557
dotnotation <dotinmotionproductions@gmail.com>
2021-11-22 17:52:39 -0500

    commit tree cc923e38bf78b4fe651b6b3d869e5832e349ee61

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 32;
+const num4 = 7;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = Math.round(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(3, 7, 18, 20);
